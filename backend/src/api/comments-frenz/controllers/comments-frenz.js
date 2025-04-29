3a30ba47f8cb4d154094a8e30a9f2cc8b666920f
Kévin <jovene.kevin@gmail.com>
2025-04-29 14:24:04 +0200

    :sparkles: Création d'un post

---
+++ backend/src/api/comments-frenz/controllers/comments-frenz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * comments-frenz controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::comments-frenz.comments-frenz');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import AddPost from './pages/AddPost'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/' element={<Home/>}/>
+        <Route path='/addpost' element={<AddPost/>}/>
       </Routes>
     </BrowserRouter>
   )
