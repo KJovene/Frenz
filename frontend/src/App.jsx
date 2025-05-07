eb9f239f3c747eba1645735dcb198029b61cbcac
lacisailles <lepage.liam@gmail.com>
2025-05-07 06:28:53 +0200

    Ajout de la route /profile vers la page de profil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import Profile from './pages/Profile' 
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
